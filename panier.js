047a7d092c3a38cd879e8ef62b11fe722126d999
Quentin Payet <quentinpayet1203@gmail.com>
2024-10-20 20:40:31 +0200

    panier.html => wip

---
+++ panier.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function () {
+  document.body.style.visibility = "visible";
+
+  const title = document.getElementById("title");
+  function addClass() {
+    title.classList.add("title-neon");
+  }
+  function rmClass() {
+    title.classList.remove("title-neon");
+  }
+  setTimeout(addClass, 300);
+  setTimeout(rmClass, 375);
+  setTimeout(addClass, 525);
+  setTimeout(rmClass, 600);
+  setTimeout(addClass, 800);
+  setTimeout(rmClass, 925);
+  setTimeout(addClass, 1325);
+});
