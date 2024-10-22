064119a05e3d4f50e782ccd25e53b4eba9443fc5
kheira <kheiraouadah90@gmail.com>
2024-10-22 10:56:34 +0200

    ajout de la section

--- apropos.js
+++ apropos.js
@@ -8,4 +8,11 @@ window.addEventListener("load", function () {
     function rmClass() {
       title.classList.remove("title-neon");
     }
+    setTimeout(addClass, 300);
+    setTimeout(rmClass, 375);
+    setTimeout(addClass, 525);
+    setTimeout(rmClass, 600);
+    setTimeout(addClass, 800);
+    setTimeout(rmClass, 925);
+    setTimeout(addClass, 1325);
 });
